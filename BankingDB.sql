ea5a4d6911f1da343fd41aec963d14ce3ec7fa7b
tanmayeepatil070 <tanmayeepatil070@gmail.com>
2026-07-28 08:53:15 +0530

    Created bankingdb database

---
+++ BankingDB.sql
@@ -0,0 +1,16 @@
+-- show databases;
+-- CREATE DATABASE BankingDB;
+-- USE Bankingdb;
+CREATE TABLE Customers(
+CustomerID INT PRIMARY KEY,
+FirstName VARCHAR(50),
+LastName VARCHAR(50),
+Email VARCHAR(100),
+Phone VARCHAR(15),
+AccountCreationDate DATE
+);
+
+describe customers;
+
+select * from customers;
+
