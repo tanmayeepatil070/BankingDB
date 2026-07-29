a4964f723c2333a1c387778849acb784dca6ef67
tanmayeepatil070 <tanmayeepatil070@gmail.com>
2026-07-29 09:16:55 +0530

    Created more Tables

--- BankingDB.sql
+++ BankingDB.sql
@@ -14,3 +14,47 @@ describe customers;
 
 select * from customers;
 
+ CREATE TABLE Accounts(
+ AccountID INT,
+ AccountType VARCHAR(50),
+ Balance DECIMAL(10,2)
+ );
+ 
+ desc accounts;
+ 
+ CREATE TABLE Transctions(
+ TransactionID INT,
+ TransactionDate DATE,
+ Amount DECIMAL (10,2),
+ TransactionType VARCHAR(20)
+ );
+ 
+ desc Transctions;
+ALTER TABLE Transctions
+RENAME TO Transactions;
+
+CREATE TABLE Branches(
+BranchID INT,
+BranchName VARCHAR(100),
+BranchAddress VARCHAR(100),
+BranchPhone VARCHAR(15)
+);
+
+desc Branches;
+
+CREATE TABLE AccountBranches(
+AssignmentDate date
+);
+
+CREATE TABLE Loans (
+LoanID INT,
+LoanAmount decimal(10,2),
+InterestRate decimal(5,2),
+StartDate DATE,
+EndDate DATE
+);
+desc loans;
+ 
+ 
+ 
+ 
