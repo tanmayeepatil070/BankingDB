777b2690e945c52a630a502067b734397aee97f1
tanmayeepatil070 <tanmayeepatil070@gmail.com>
2026-07-30 09:08:09 +0530

    Used ALTER

--- BankingDB.sql
+++ BankingDB.sql
@@ -54,7 +54,58 @@ StartDate DATE,
 EndDate DATE
 );
 desc loans;
+
+ALTER TABLE customers
+ADD DateOfBirth DATE;
+desc customers;
+
+ALTER TABLE customers
+MODIFY Phone VARCHAR(20);
+desc customers;
+
+ALTER TABLE Accounts
+ADD CONSTRAINT chk_MinBalance
+CHECK (Balance >= 1000);
+ desc Accounts;
  
+ DROP TABLE AccountBranches;
+ 
+ ALTER TABLE Accounts
+ ADD CustomerID INT;
+ 
+ ALTER TABLE Accounts
+ ADD CONSTRAINT Fk_Accounts_Customers
+ foreign key (CustomerID)
+ REFERENCES Customers(CustomerID);
+ desc Accounts;
+ 
+ ALTER TABLE Accounts
+ ADD Constraint PK_AccountID
+ PRIMARY KEY (AccountID);
+ desc accounts;
+ 
+ALTER TABLE customers
+MODIFY FirstName varchar(50) NOT NULL;
+desc customers;
+
+ALTER TABLE Customers
+ADD constraint uq_Email
+UNIQUE (Email);
+desc Customers;
+ 
+ ALTER TABLE Branches
+ ADD constraint PK_BranchID
+ primary key (BranchID);
+ desc branches;
+ 
+ Alter table Accounts
+ ADD BranchID INT;
+
+  ALTER TABLE Accounts
+ ADD CONSTRAINT Fk_Branch_Customers
+ foreign key (BranchID)
+ REFERENCES Branches(BranchID);
+ desc Accounts;
  
  
  
